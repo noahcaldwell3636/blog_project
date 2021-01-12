971ec688e139f1071aae3fb8f11bf8e153bc704a
Noah Caldwell <noahcaldwell36@gmail.com>
2021-01-11 22:33:53 -0500

    fixed fixed some login stuuf i think... about to continue the tutorial

--- social/accounts/urls.py
+++ social/accounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [ 
     url(r'my_account/$', account_views.my_account.as_view(), name='social-main'),
-    url(r'login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    url(r'logout/$', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
-    url(r'signup/$', account_views.sign_up.as_view(), name='signup'),
+    url(r'login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='social-login'),
+    url(r'logout/$', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='social-logout'),
+    url(r'signup/$', account_views.sign_up.as_view(), name='social-signup'),
 ]
