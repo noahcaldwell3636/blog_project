cd26c59909d9b4de94a7c343ddf69ab7e3b5be53
Noah Caldwell <noahcaldwell36@gmail.com>
2021-02-23 22:48:18 -0500

    remove comment link on same line as author

---
+++ blog/templatetags/my_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from blog.forms import CommentForm
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from ..models import Post
+
+register = template.Library()
+
+@login_required
+@register.inclusion_tag('blog/_comment_form.html')
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return {"form": form, "post": post}
+
